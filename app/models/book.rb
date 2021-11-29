8a8a04faa622344ece67eb43a356f99d8f789a6c
EC2 Default User <ec2-user@ip-172-31-4-180.ec2.internal>
2021-11-29 08:18:21 +0000

    bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,11 @@
 class Book < ApplicationRecord
-	belongs_to :user
-	has_many :book_comments, dependent: :destroy
+  belongs_to :user
 	has_many :favorites, dependent: :destroy
-	
-  def favorited_by?(user_id)
-     favorites.where(user_id: user.id).exists?
-  end
+	has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
 
-	validates :title, presence: true, length: { maximum: 50 }
-	validates :body, presence: true, length: {maximum: 200}
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
 end
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -2,5 +2,7 @@ class BookComment < ApplicationRecord
 
   belongs_to :book
   belongs_to :user
+  
+  validates :comment, presence: true
 
 end
